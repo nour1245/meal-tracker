e52f5bd1cd42549c0315428fb11ef18f31bf67c1
nour1245 <aldennour62@gmail.com>
2025-03-26 14:08:46 +0200

    Integrate Sentry for error tracking and monitoring in the application

--- lib/core/helpers/services.dart
+++ lib/core/helpers/services.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:mealtracker/features/home/data/meal_model.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 setupServices() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter(MealModelAdapter());
+}
+
+setupSentry() async {
+  await SentryFlutter.init((options) {
+    options.tracesSampleRate = 0.1;
+    options.dsn =
+        'https://a97ff2466b63ccd511b8526b82cc8ffe@o4509043946487808.ingest.de.sentry.io/4509043947602000';
+  });
 }
